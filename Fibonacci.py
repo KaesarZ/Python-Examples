e1129491ee95d133b43e3e549f3c3f5308929087
KaesarZ <jccb2@cin.ufpe.br>
2019-01-29 07:38:16 -0300

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,32 @@
+'''
+        Project: Fibonacci
+        Description: Calcula o n-zimo termo da sequencia de fibonacci recursivamente
+'''
+def fib(num, dic={}):
+    ''' Retorna o valor do n-zimo termo da sequencia de fibonacci recursivamente'''
+    if num < 2:
+        return num
+    elif num in dic:
+        return dic[num]
+    else:
+        res = fib(num - 1, dic) + fib(num - 2, dic)
+        if res not in dic:
+            dic[num] = res
+        return res
+    
+def inputValidInt(label):
+    ''' Retorna um inteiro válido digitado pelo usuário '''
+    try:
+        num = int(float(input(label)))
+    except:
+        print('Erro: a entrada precisa ser um inteiro.')
+        return inputValidInt(label)
+    return num
+
+def main():
+    ''' Função Inicial '''
+    print('------------- FIBONACCI --------------')
+    num = inputValidInt('Digite um número inteiro: ')
+    print('fibonacci(' + str(num) + ') =', fib(num))
+
+main()
