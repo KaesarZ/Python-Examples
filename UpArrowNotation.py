ec3abeed3f7ac7e6c9bba1c7e77e7097b8e726e6
KaesarZ <jccb2@cin.ufpe.br>
2019-01-29 07:38:35 -0300

    Create UpArrowNotation.py

---
+++ UpArrowNotation.py
@@ -0,0 +1,18 @@
+﻿'''
+        Project: Notação de Knuth
+        Description: Retorna o resultado decimal dada uma expressão na notação de Knuth's Up-Arrow Notation
+'''
+num = int(input('Digite a base: '))
+qnt = int(input('Digite a quantidade de up-arrows:'))
+exp = int(input('Digite a quantidade de expoentes: '))
+
+def multiarrows(num=0, qnt=1, exp=0):
+    if qnt == 1:
+        return num ** exp
+    result = num
+    for _ in range(exp - 1):
+        result = multiarrows(num, qnt - 1, result)
+    return result
+
+print(multiarrows(num,qnt,exp))
+
