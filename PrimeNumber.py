d45a80f04dbc999c469741d43516df5ae7c46504
KaesarZ <jccb2@cin.ufpe.br>
2019-01-29 07:38:30 -0300

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,19 @@
+﻿'''
+        Project: Número Primo
+        Description: Retorna se o número parametro é primo
+        Name: Júlio César de Carvalho Barros
+        Email: jccb2@cin.ufpe.br
+'''
+num = int(input("Digite um número inteiro positivo: "))
+
+def isPrime(num):
+    ''' Retorna o resultado booleano da verificação de número primo'''
+    prime = True if num > 2 else False
+    count = 2
+    while(count <= int(num ** 0.5) and prime):
+        if(num % count == 0):
+            prime = False
+        count += 1
+    return prime
+
+print(isPrime(num))
